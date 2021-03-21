c042f4543f6d3ea58303673216c6e0ef345bf451
Felix Cammaerts <felix.cammaerts@hotmail.com>
2021-03-21 01:11:00 +0100

    Finished search bar

--- imports/ui/Books.jsx
+++ imports/ui/Books.jsx
@@ -1,57 +1,36 @@
 import React from 'react';
 import {BooksCollection, SimilarBooksCollection} from '../api/links';
 import {RoutePaths} from "./RoutePaths";
-import {Link, Route} from "react-router-dom";
-import ReactDOM from 'react-dom';
+import {Link} from "react-router-dom";
 import SearchBar from "material-ui-search-bar";
 import {recommendedBooks, updateRecommendations} from "../utils/utils";
-import * as d3 from "d3";
 
 export var wantToReadBooks = [];
 export var ratings = {};
 export var genresMap = {};
 
-function search(value) {
-    console.log(value);
-}
+function setBooks(books, search, prevSearch){
+    if (prevSearch.length > search.length){
 
-function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-}
+    }
+    let titles = []
+    const new_books = BooksCollection.find({"title" : {$regex : ".*"+search+".*", $options: 'i'}}).fetch();
+    new_books.forEach(book =>
+        titles.push(book["title"])
+    )
 
-function setBooks(books, search){
-
-    const regex = new RegExp(escapeRegex(search), 'gi');
-    books = BooksCollection.find({"title":regex}).fetch();
     const node = document.getElementById("books");
-    ReactDOM.unmountComponentAtNode(node);
-    node.innerHTML = "";
-   // node.innerHTML = '';
-    //const element = books.forEach(book => {"<p>" + book["title"] + "</p>"});
-    console.log(books[0]["title"]);
-    //const element = <p> {books[0]["title"]}</p>;
-   // const element = <h1>test</h1>;
-   // ReactDOM.render(element, document.getElementById('books'))
-    let cards = [];
-    books.forEach(book => cards.push(
-            <Route to={RoutePaths.BOOK + "/" + book["isbn"]}>
-                <div className="column">
-                    <div className="card">
-                        <img id={book["isbn"]} src={book["image_url"]} width="98" height="146" alt="Unable to load image"/>
-                        <div className="container">
-                            <p><small>{book["title"]}</small></p>
-                        </div>
-                    </div>
-                </div>
-            </Route>
-        ));
-    console.log(cards);
-    const parent = document.getElementById('books');
-    ReactDOM.render(<div>{cards}</div>, parent);
-//        const parent = document.getElementById('books');
-//         ReactDOM.render(element, parent);
-    //ReactDOM.render(element, document.getElementById('books'));
 
+    const children = node.childNodes;
+    children.forEach(child =>
+        {
+            if (!titles.includes(child.id)){
+                child.style.display = "none";
+            } else {
+                child.style.display = "block";
+            }
+        }
+    )
 }
 
 export const getGenresFromID = (genres, id) => {
@@ -75,7 +54,6 @@ export const Books = () => {
     if (recommendedBooks.length !== 0){
         books = recommendedBooks;
     }
-    console.log(books);
     const visualization =  document.getElementById("visualization");
     if (visualization !== null) {
         visualization.parentNode.removeChild(visualization);
@@ -86,13 +64,16 @@ export const Books = () => {
     if (visualization_par !== null) {
         visualization_par.parentNode.removeChild(visualization_par);
     }
-    console.log(d3);
+    let prevSearch = "";
     return (
         <div>
             <div>
                 <SearchBar
-                    onChange={(newValue) => setBooks(books, newValue)}
-                    onRequestSearch={search}
+                    onChange={(newValue) => {
+                        setBooks(books, newValue, prevSearch);
+                        prevSearch = newValue;
+                        }
+                    }
                     placeholder="Title, author, keyword or ISBN"
                     autoFocus
                 />
@@ -100,7 +81,7 @@ export const Books = () => {
             <div className="grid-container" id="books">
                 {
                     books.map(book =>
-                    <div className="grid-item">
+                    <div className="grid-item" id={book["title"]}>
                         <Link to={RoutePaths.BOOK + "/" + book["isbn"]}>
                             <img id={book["isbn"]} src={book["image_url"]} width="98" height="146" alt="Unable to load image"/>
                         </Link>
