34d24d38e89c8cd48bb6a82397598ab7b4dcb05b
Felix Cammaerts <felix.cammaerts@hotmail.com>
2021-04-02 02:17:49 +0200

    updated tooltip InitializationBooks.jsx

--- app/imports/ui/InitializationBooks.jsx
+++ app/imports/ui/InitializationBooks.jsx
@@ -8,7 +8,7 @@ import {Link} from "react-router-dom";
 
 export const InitializationBooks = () => {
     const classes = useStyles();
-    const books = BooksCollection.find({"isbn":{$ne : "isbn"}},{limit: 200}).fetch();
+    const books = BooksCollection.find({"isbn":{$ne : "isbn"}},{limit: 50}).fetch();
     return (
         <div>
             <div className="grid-container">
