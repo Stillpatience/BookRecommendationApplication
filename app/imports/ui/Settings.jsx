99a1da5088c03588264330fed2309c6e16f9d426
Felix Cammaerts <felix.cammaerts@hotmail.com>
2021-04-16 14:09:36 +0200

    using snackbar as feedback in Settings.jsx now

--- app/imports/ui/Settings.jsx
+++ app/imports/ui/Settings.jsx
@@ -5,25 +5,44 @@ import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import {Genres} from './Genres'
-import {useStyles} from "./styles";
-
+import {Snackbar} from "@material-ui/core";
+import {Alert} from "@material-ui/lab";
 
 export const Settings = (props) => {
-    const classes = useStyles();
 
+    const handleClose = () => {
+        setOpen(false);
+        const snackbar = document.getElementById("settings-snackbar");
+        snackbar.style.display = "none";
+    }
+
+    const handleClick = () => {
+        setOpen(true);
+        const snackbar = document.getElementById("settings-snackbar");
+        snackbar.style.display = "block"
+    }
+    const [open, setOpen] = React.useState(false);
     return (
-        <div className={classes.root}>
-            <FormControl component="fieldset" className={classes.formControl}>
+        <div >
+            <FormControl component="fieldset">
                 <FormLabel component="legend">Stop receiving recommendations for following genres: </FormLabel>
                 <FormGroup>
                     {Genres.map(genre =>
                     <FormControlLabel
                         control={<Checkbox checked={props.genreStates[genre]}
                                            onChange={props.handleGenreChange}
+                                           onClick={handleClick}
                                            color="primary"
                                            name={genre}/>}
                         label={genre}
                     />)}
+                    <div id="settings-snackbar" style={{display :"none"}}>
+                        <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+                            <Alert onClose={handleClose} severity="success">
+                                Updated preferences!
+                            </Alert>
+                        </Snackbar>
+                    </div>
                 </FormGroup>
             </FormControl>
         </div>
