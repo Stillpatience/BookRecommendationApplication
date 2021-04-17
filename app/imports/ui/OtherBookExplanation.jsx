64167a246c7096d6a7a661908faf6f90443f9a14
Felix Cammaerts <felix.cammaerts@hotmail.com>
2021-04-17 18:19:35 +0200

    updated layout of OtherBookExplanation.jsx

--- app/imports/ui/OtherBookExplanation.jsx
+++ app/imports/ui/OtherBookExplanation.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import * as d3 from "d3";
 import {BooksCollection} from "../api/links";
-import {genresMap} from "./Books";
+import {genresMap, getShortTitle} from "./Books";
 
 class OtherBookExplanation extends Component {
     componentDidMount() {
@@ -9,22 +9,11 @@ class OtherBookExplanation extends Component {
     }
 
     drawChart(){
-
-        const w = window.innerWidth;
-        const h = "60rem";
-        const svg = d3.select("#navigation")
-            .insert("svg",":first-child")
-            .attr("width", w)
-            .attr("height", h)
-            .attr("id", "other-books")
-            .style("margin-left", 100);
-
         const genresToBooks = {};
         for (const key in genresMap) {
             if (genresMap.hasOwnProperty(key)) {
                 const genres = genresMap[key];
                 genres.forEach(genre => {
-                    console.log("key", key);
                     if (typeof genresToBooks[genre] == 'undefined'){
                         genresToBooks[genre] = [key]
                     }else{
@@ -33,53 +22,89 @@ class OtherBookExplanation extends Component {
                 })
             }
         }
+        const w = window.innerWidth;
+        const h = ((17 * Object.keys(genresToBooks).length)  + 3) + "rem";
+
+        const svg = d3.select("#navigation")
+            .insert("svg",":first-child")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("id", "other-books")
+            .style("margin-left", 100);
+
+
         let y = 2
         svg.append("text")
             .attr("x", 0)
             .attr("y", "1em")
             .attr("dy", ".35em")
             .attr("font-size", "1em")
+            .style("font-family" , '"Roboto", "Helvetica", "Arial", sans-serif')
             .text("Other books you read contain the same genres: ");
 
         for (const key in genresToBooks) {
             if (genresToBooks.hasOwnProperty(key)) {
                 const books = genresToBooks[key];
-                svg.append("text")
+                let y_title = y
+                const fo = svg.append("foreignObject")
                     .attr("x", 0)
-                    .attr("y", y + "em")
+                    .attr("y", y_title + "rem")
+                    .attr("width", w)
+                    .attr("height", 300)
+                    .append('xhtml:div')
+                    .attr("text-overflow", "ellipsis")
+                    .attr("overflow-wrap", "anywhere")
+                    .attr("overflow", "hidden")
+
+
+                fo.append("p")
+                    .attr('x', 0)
+                    .attr('y', y_title + "rem")
                     .attr("dy", ".35em")
-                    .attr("font-size", "1em")
-                    .attr("font-weight", "bold")
-                    .attr("id", key + "title")
-                    .attr("overflow", "ellipsis")
-                    .text(key);
+                    .style("font-family" , '"Roboto", "Helvetica", "Arial", sans-serif')
+                    .style("font-size", "1.2rem")
+                    .style("font-weight", "bold")
+                    .text(key)
 
                 let x_img = 0
                 books.forEach(book_id => {
                     let book = BooksCollection.find({"id":parseInt(book_id)}, {}).fetch();
                     let image_url = "/" + book_id + ".jpg";
-                    let book_title = book[0]["title"];
+                    let book_title = getShortTitle(book[0]);
 
-                    let y_img = y + 2;
+                    let y_img = y + 3;
                     svg.append("svg:image")
-                        .attr('x', x_img + "em")
-                        .attr('y', y_img + "em")
+                        .attr('x', x_img + "rem")
+                        .attr('y', y_img + "rem")
                         .attr('width', 98)
                         .attr('height', 146)
                         .attr("xlink:href", image_url)
 
-                    let y_text = y_img + 10;
-                    svg.append("text")
-                        .attr('x', x_img + "em")
-                        .attr('y', y_text + "em")
+                    let y_text = y_img + 9;
+
+                    const fo = svg.append("foreignObject")
+                        .attr("x",  x_img + "rem")
+                        .attr("y", y_text + "rem")
+                        .attr("width", w)
+                        .attr("height", 300)
+                        .append('xhtml:div')
+                        .style("width", "98px")
+                        .style("height", "98px")
+                        .attr("text-overflow", "ellipsis")
+                        .attr("overflow-wrap", "anywhere")
+                        .attr("overflow", "hidden")
+
+
+                    fo.append("p")
+                        .attr('x', x_img + "rem")
+                        .attr('y', y_text + "rem")
                         .attr("dy", ".35em")
-                        .attr("text-length", "6em")
+                        .style("font-family" , '"Roboto", "Helvetica", "Arial", sans-serif')
                         .attr("font-size", "1em")
                         .text(book_title)
-                    x_img += 14
-
+                    x_img += 7
                 })
-                y += 15
+                y += 17
 
             }
         }
